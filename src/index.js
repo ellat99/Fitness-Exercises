93e61da47a47e8dd7aa151a6e4cf09e3f85a38aa
Talmaciu Elena <148704214+ellat99@users.noreply.github.com>
2024-02-22 18:49:50 +0200

    searchexercises

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
 import './components/App.module.css';
-import './index.css';
+
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
