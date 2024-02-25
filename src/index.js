254a7491964bba6448c6c418b24416029b06173e
Talmaciu Elena <148704214+ellat99@users.noreply.github.com>
2024-02-25 16:34:14 +0200

    up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'components/App';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
 import './components/App.module.css';
 
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
 import { Box } from '@mui/material';
+
 import Exercises from '../components/Exercises/Exercises';
-import SearchExercises from 'components/SearchExercises/SearchExercises';
-import HeroBanner from 'components/HeroBanner/HeroBanner';
+import SearchExercises from '../components/SearchExercises/SearchExercises';
+import HeroBanner from '../components/HeroBanner/HeroBanner';
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   );
 };
+
 export default Home;
